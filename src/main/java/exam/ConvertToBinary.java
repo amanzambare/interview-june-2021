152ef9868d2aa269c7ca37d840922b994b0256d9
amanzambare <76624676+amanzambare@users.noreply.github.com>
2021-06-22 12:39:57 +0530

    commit

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,6 +3,7 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
+    int bin[]
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     return "";
   }
--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,34 @@
 package exam;
-
+import java.util.*;
+import java.lang.String;
+import java.lang.System;
 public class ShiftElementsInArray {
 
-  public void shift(int[] arr) {
+  public  void shift(int[] arr) {
+    int n=arr.length;
+    int temp=arr[n-1];
+    for(int i=n-1;i>0;i--)
+    {
+      arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
 
+  for(int i=0;i<n;i++)
+  {
+    System.out.println(arr[i]);
+  }
   }
 
+      public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+              arr[i]=sc.nextInt();
+        }
+        ShiftElementsInArray obj=new ShiftElementsInArray();
+        obj.shift(arr);
+      }
 }
